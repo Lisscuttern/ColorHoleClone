234fcd7d00b2bd7f159a25b10be9ed6fc6d99c1b
Ozan <73613081+Lisscuttern@users.noreply.github.com>
2022-06-11 15:52:02 -0700

    ++ ADD : Collect Cubes

--- ColorHole/Assets/Scripts/Components/CubeComponent.cs
+++ ColorHole/Assets/Scripts/Components/CubeComponent.cs
@@ -5,9 +5,7 @@ using MyNameSpace;
 
 public class CubeComponent : MonoBehaviour
 {
-    [SerializeField] private HoleComponent m_holeComponent;
     [SerializeField] private Transform m_transform;
-    [SerializeField] private Transform m_hole;
     [SerializeField] private Rigidbody m_rb;
     [SerializeField] private GameSettings m_gameSettings;
 
@@ -16,11 +14,14 @@ public class CubeComponent : MonoBehaviour
         CheckDistance();
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider other)
     {
-        if (collision.gameObject.tag == CommonTypes.TAG_HOLE)
+        if (other.gameObject.tag == CommonTypes.TAG_HOLE)
         {
-            Destroy(gameObject,1.5f);
+            transform.DOJump(new Vector3(m_transform.position.x, m_transform.position.y, m_transform.position.z),1,1,0.5f).OnComplete(()=>
+            {
+                Destroy(gameObject);
+            });
         }
     }
 
@@ -32,8 +33,10 @@ public class CubeComponent : MonoBehaviour
         Vector3 holeCenterPoint = new Vector3(m_transform.position.x, m_transform.GetComponent<SphereCollider>().bounds.max.y, m_transform.position.z);
         Vector3 direction = holeCenterPoint - transform.position;
         
+        
         if (Vector3.Distance(holeCenterPoint, transform.position) < m_gameSettings.Distance)
         {
+            Debug.Log(Vector3.Distance(holeCenterPoint,transform.position));
             m_rb.AddForce(m_gameSettings.FallForce * direction, ForceMode.Force);
         }
     }
